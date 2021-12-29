5c0cbe1a4d4735fc50fb24241b22710cb988813d
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2021-12-29 17:48:16 +0100

    Add Title and animations

--- src/app/components/component-detail/component-detail.component.ts
+++ src/app/components/component-detail/component-detail.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentDetailService } from 'src/app/services/component-detail.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-component-detail',
   templateUrl: './component-detail.component.html',
   styleUrls: ['./component-detail.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0.2,
+        })
+      ),
+      transition('void <=> *', animate(1500)),
+    ]),
+  ],
 })
 export class ComponentDetailComponent implements OnInit {
   panelOpenState: boolean = false;
--- src/app/components/component-list/component-list.component.ts
+++ src/app/components/component-list/component-list.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentListService } from 'src/app/services/component-list.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-component-list',
   templateUrl: './component-list.component.html',
   styleUrls: ['./component-list.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0.2,
+        })
+      ),
+      transition('void <=> *', animate(1500)),
+    ]),
+  ],
 })
 export class ComponentListComponent implements OnInit {
   images: any[] = [];
